a0c1043ed197caf486a6da30d9121ff2ba325826
deepanshu <deepanshu@tothenew.com>
2019-06-11 22:53:43 +0530

    app updated with dashboard

--- app.js
+++ app.js
@@ -1,20 +1,37 @@
 const express=require('express');
-const authRoutes=require('./routes/auth-routes');
+const authRoutes=require('./routes/auth-route');
+const profileRoutes=require('./routes/profile-route');
 const passportSetup=require('./config/passport-setup');
 const mongoose=require('mongoose');
+const cookieSession=require('cookie-session');
+const passport=require('passport');
+const keys=require('./config/keys');
+const buzzRoutes=require('./routes/buzz-route');
+const bodyParser=require('body-parser');
+const cors=require('cors');
+
 const app=express();
 
 //setting up the view engine
 app.set('view engine','ejs');
 
+// app.use(cookieSession({
+//     maxAge: 24 * 60 * 60 * 1000,
+//     keys: [keys.session.cookieKey]
+// }));
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(cors());
+// Initialize passport
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.get('/',(req,res)=>{
-    res.render('home');
-});
 //connecting mongoose
 mongoose.connect('mongodb://localhost:27017/ttn_buzz_app',{useNewUrlParser:true});
 //setup routes
+
 app.use('/auth',authRoutes);
+app.use('/profile',profileRoutes);
+app.use('/dashboard',buzzRoutes);
 
 app.listen(8080,()=>{
     console.log('server started at port number 8080');
