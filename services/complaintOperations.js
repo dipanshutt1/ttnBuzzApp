6bbbd96e7492c939b1dd165ef52865fe91ecbd7a
deepanshu <deepanshu@tothenew.com>
2019-06-14 18:03:47 +0530

    complaint insert and fetch operation added

---
+++ services/complaintOperations.js
@@ -0,0 +1,14 @@
+// const complaint= require('../models/complaint');
+//
+// complaintFire=(complaint)=>{
+//     return complaint.save();
+// }
+//
+// findComplaint=()=>{
+//     return complaint.find();
+// }
+//
+// module.exports={
+//     complaintFire,
+//     findComplaint
+// }
