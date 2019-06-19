4eaee8050ee77ac6839bc713a829c4af95fcd0a9
deepanshu <deepanshu@tothenew.com>
2019-06-19 16:16:21 +0530

    role condition is applied

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -2,6 +2,17 @@ const passport=require('passport');
 const GoogleStrategy=require('passport-google-oauth20');
 const keys=require('./keys');
 const User=require('../models/user');
+var roleData=require('../models/role');
+
+passport.serializeUser((user,done)=>{
+    done(null,user.id);
+})
+
+passport.deserializeUser((id,done)=>{
+    User.findById(id).then((user)=>{
+        done(null,user);
+    });
+})
 
 passport.use(new GoogleStrategy({
     //options for google strategy
@@ -10,10 +21,25 @@ passport.use(new GoogleStrategy({
     clientSecret:keys.google.clientSecret
 },(accessToken,refreshToken,profile,done)=>{
     //passport callback function to check if the user already exists in the db
-    console.log(profile.sub);
+    console.log(profile);
+    var role;
+    console.log("role is ",roleData.role.IT);
+    if(profile._json.email===roleData.role.HR){
+        role='HR'
+    }
+    else if(profile._json.email===roleData.role.IT){
+        role='IT'
+    }
+    else if(profile._json.email===roleData.role.INFRASTRUCTURE){
+        role='INFRASTRUCTURE'
+    }
+    else{
+        role='USER'
+    }
     User.findOne({googleId:profile.id}).then((currentUser)=>{
         if(currentUser){
             console.log('user is:',currentUser);
+            done(null,currentUser);
         }
         else{
             //if the user doesn't exist create new user in our db
@@ -21,9 +47,11 @@ passport.use(new GoogleStrategy({
                 userName:profile._json.name,
                 googleId:profile.id,
                 userImg:profile._json.picture,
-                email:profile._json.sub
+                email:profile._json.email,
+                userRole:role
             }).save().then((newUser)=>{
                 console.log('new user created'+newUser);
+                done(null,newUser);
             })
         }
     })
