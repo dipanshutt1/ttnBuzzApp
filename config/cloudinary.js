f470bde7ce4f172ed9827113b8ef404edffab1e9
deepanshu <deepanshu@tothenew.com>
2019-06-19 16:24:30 +0530

    updated

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -4,7 +4,7 @@ const keys=require('./keys');
 cloudinary.config({
     cloud_name:keys.cloudinary.cloud_name,
     api_key:keys.cloudinary.api_key,
-    secret_key: keys.cloudinary.secret_key
+    api_secret: keys.cloudinary.secret_key
 })
 
 module.exports=cloudinary;
