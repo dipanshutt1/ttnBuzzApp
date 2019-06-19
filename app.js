10864f179131b6c33a1594109aa4dc3efc9892e9
deepanshu <deepanshu@tothenew.com>
2019-06-19 16:22:51 +0530

    updated for role

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const express=require('express');
-const authRoutes=require('./routes/auth-route');
-const profileRoutes=require('./routes/profile-route');
+const authRoutes=require('./routes/auth');
+const profileRoutes=require('./routes/profile');
 const passportSetup=require('./config/passport-setup');
 const mongoose=require('mongoose');
 const cookieSession=require('cookie-session');
 const passport=require('passport');
 const keys=require('./config/keys');
-const buzzRoutes=require('./routes/buzz-route');
+const buzzRoutes=require('./routes/buzz');
+const complaintRoutes=require('./routes/complaint');
+const userProfileRoutes=require('./routes/userProfile');
 const bodyParser=require('body-parser');
 const cors=require('cors');
-
 const app=express();
+require('./config/cloudinary');
 
 //setting up the view engine
 app.set('view engine','ejs');
@@ -32,6 +34,8 @@ mongoose.connect('mongodb://localhost:27017/ttn_buzz_app',{useNewUrlParser:true}
 app.use('/auth',authRoutes);
 app.use('/profile',profileRoutes);
 app.use('/dashboard',buzzRoutes);
+app.use('/dashboard',complaintRoutes);
+app.use('/dashboard',userProfileRoutes);
 
 app.listen(8080,()=>{
     console.log('server started at port number 8080');
