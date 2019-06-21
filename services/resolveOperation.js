43dcc3fa468cd1a963ccd175a0b98e27ea102ec8
deepanshu <deepanshu@tothenew.com>
2019-06-21 18:16:26 +0530

    mycomplaint operation added

--- services/resolveOperation.js
+++ services/resolveOperation.js
@@ -2,13 +2,17 @@ const complaint=require('../models/complaint');
 const roleData=require('../models/role');
 
 const findAllComplaints=()=>{
-    return complaint.find();
+    return complaint.find().sort({'issue_date':-1})
 }
 const changeStatus=(complaintId,status)=>{
     return complaint.update(
         {id:complaintId},{status:status});
 }
+const findMyComplaint=(email)=>{
+    return complaint.find({assigned_email:email}).sort({'issue_date':-1})
+}
 module.exports={
     findAllComplaints,
-    changeStatus
-} 
+    changeStatus,
+    findMyComplaint
+}
