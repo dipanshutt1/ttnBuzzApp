a336b76597fb8aa292d60e07829f15e129a4bee4
dipanshutt1 <deepanshu@tothenew.com>
2019-06-29 14:01:46 +0530

    user roledeactivated done

--- routes/auth.js
+++ routes/auth.js
@@ -1,32 +1,36 @@
-const router=require('express').Router();
-const passport=require('passport');
-const jwt=require('jsonwebtoken');
-const keys=require('../config/keys');
+const router = require('express').Router();
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const keys = require('../config/keys');
 
 //auth with google
-router.get('/google',passport.authenticate('google',{
-    scope:['profile','email']
-})
+router.get('/google', passport.authenticate('google', {
+        scope: ['profile', 'email']
+    })
     // console.log(req.user);
 );
 
 //callback route for redirecting from google
-router.get('/google/redirect',passport.authenticate('google', {
+router.get('/google/redirect', passport.authenticate('google', {
     failureRedirect: '/profile/'
-}),(req,res)=>{
+}), (req, res) => {
     // res.send(req.user);
-    jwt.sign(req.user.toJSON(),keys.session.cookieKey,{expiresIn:'10h'},
-        function (err,token) {
-            if(err){
-                console.log('err: ${err}');
-            }
-            else{
-                console.log(`token:${token}`);
-                res.redirect(`http://localhost:3000/token?q=${token}`);
-            }
+    if (req.user.status === 'Deactivate') {
+        res.redirect(`http://localhost:3000/accountError`);
+    } else {
+        jwt.sign(req.user.toJSON(), keys.session.cookieKey, {expiresIn: '10h'},
+            function (err, token) {
+                if (err) {
+                    console.log('err: ${err}');
+                } else {
+                    console.log(`token:${token}`);
+                    res.redirect(`http://localhost:3000/token?q=${token}`);
+                }
 
-        });
+            });
+    }
+
     // res.redirect('/profile/');
 })
 
-module.exports=router;
+module.exports = router;
